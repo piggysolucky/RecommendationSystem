a1f561f9768681afb625d1576180aa19827999c3
Lu <chen.4728@osu.edu>
2016-04-04 19:19:40 -0400

    commit test

--- Video_Recommendation.py
+++ Video_Recommendation.py
@@ -1,21 +1,20 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-#give the input
+# give the input
 # Parse the html , and store it in Beautiful Soup format
-history = BeautifulSoup(open("/Users/Yn/Documents/CSE5522/Final/History_Page.html"), "lxml")
+history = BeautifulSoup(open("History_Page.html"), "lxml")
 
-#test
-#print history.prettify()
+# test
+# print history.prettify()
 
-title=""
-#get the names of videos, and add all of them into a long string "title"
-for video in history.findAll('a', attrs={'class':'yt-uix-tile-link'}):
-    title=title+" "+video.get('title')
 
-#a set that is used to store keywords. 1. Tokenize "title"  2. remove duplicate
-name=set(title.split())
+# get the names of videos, and add all of them into a long string "title"
+videos = history.findAll('a', attrs={'class': 'yt-uix-tile-link'})
+titles = [v.get('title') for v in videos]
 
-#test
+# a set that is used to store keywords. 1. Tokenize "title"  2. remove duplicate
+name = set(" ".join(titles).split())
+
+# test
 print name
-
