62bcb8e74b8865f10aefa99c9ee7c7357a8c1dbb
piggysolucky <erinyan1992@gmail.com>
2016-04-19 19:49:37 -0400

    basic model to get information on a channel using the api key

--- Video_Recommendation.py
+++ Video_Recommendation.py
@@ -1,20 +1,5 @@
-import urllib2
-from bs4 import BeautifulSoup
-
-# give the input
-# Parse the html , and store it in Beautiful Soup format
-history = BeautifulSoup(open("History_Page.html"), "lxml")
-
-# test
-# print history.prettify()
-
-
-# get the names of videos, and add all of them into a long string "title"
-videos = history.findAll('a', attrs={'class': 'yt-uix-tile-link'})
-titles = [v.get('title') for v in videos]
-
-# a set that is used to store keywords. 1. Tokenize "title"  2. remove duplicate
-name = set(" ".join(titles).split())
-
-# test
-print name
+import urllib, json
+url = "https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=UCBkNpeyvBO2TdPGVC_PsPUA&key=AIzaSyDoXGmy_RpUY1cUYWCceN2kSWT4-vDZOaE"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+print data
