a98ff609d3fb6f7883c065effa6e530ff35dbac0
piggysolucky <erinyan1992@gmail.com>
2016-04-20 17:42:15 -0400

    get all the video names

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ See reference for how to use Youtuve Data API:https://developers.google.com/yout
 http://stackoverflow.com/questions/26831919/get-all-playlist-ids-from-channel-id-youtube-api-v3
 https://developers.google.com/youtube/v3/docs/playlists/list#parameters
 playlistitem:
-https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLB03EA9545DD188C3&key=MY_API_KEY
+https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLB03EA9545DD188C3&key=MY_API_KEY
 
 basically use https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=UCBkNpeyvBO2TdPGVC_PsPUA&key=AIzaSyDoXGmy_RpUY1cUYWCceN2kSWT4-vDZOaE
 (api key is the key above), we can retrive the api key of a channel
--- Video_Recommendation.py
+++ Video_Recommendation.py
@@ -2,12 +2,21 @@ import urllib
 import json
 #Get top 1000 users on youtube
 
-#Get the video titles in a channel's playlist
+#Get the playlistIds in a channel's playlist
 url = "https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=UCBkNpeyvBO2TdPGVC_PsPUA&key=AIzaSyDoXGmy_RpUY1cUYWCceN2kSWT4-vDZOaE"
 response = urllib.urlopen(url)
 channelPlaylistInfo = json.loads(response.read())
 channelPlaylist = channelPlaylistInfo["items"]
-channelSnippets = [item["snippet"] for item in channelPlaylist]
-channelTitles = [snippet["title"] for snippet in channelSnippets]
-print channelTitles
+channelPlaylistIds = [item["id"] for item in channelPlaylist]
 
+#Get the video titles in the playlist
+titles = []
+for playlistId in channelPlaylistIds:
+    url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId="+ playlistId + "&key=AIzaSyDoXGmy_RpUY1cUYWCceN2kSWT4-vDZOaE"
+    response = urllib.urlopen(url)
+    channelVideolistInfo = json.loads(response.read())
+    videoList = channelPlaylistInfo["items"]
+    videoListSnippets = [item["snippet"] for item in videoList]
+    titles += [item["title"] for item in videoListSnippets]
+
+print titles
