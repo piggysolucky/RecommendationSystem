3a7632e6f7caf11f7165a82a5b47929b631651e6
piggysolucky <erinyan1992@gmail.com>
2016-04-20 00:59:33 -0400

    get the playlist titles of a user

--- Video_Recommendation.py
+++ Video_Recommendation.py
@@ -1,5 +1,9 @@
-import urllib, json
+import urllib
+import json
 url = "https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=UCBkNpeyvBO2TdPGVC_PsPUA&key=AIzaSyDoXGmy_RpUY1cUYWCceN2kSWT4-vDZOaE"
 response = urllib.urlopen(url)
-data = json.loads(response.read())
-print data
+channelPlaylistInfo = json.loads(response.read())
+channelPlaylist = channelPlaylistInfo["items"]
+channelSnippets = [item["snippet"] for item in channelPlaylist]
+channelTitles = [snippet["title"] for snippet in channelSnippets]
+print channelTitles
